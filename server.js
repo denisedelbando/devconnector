84000cc20ab432ce0ccc5e598d655004706ef040
denisedelbando <denise.delbando@gmail.com>
2021-04-24 17:53:39 -0400

    Create routes

--- server.js
+++ server.js
@@ -4,9 +4,18 @@ const connectDB = require('./config/db');
 
 
 const app = express();
+
+//* CONNECT DB
 connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
+
+//* DEFINE ROUTES 
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/auth', require('./routes/api/auth'));
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
