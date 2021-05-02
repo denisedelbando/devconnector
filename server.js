fd00a6de65193e64e961f3b98ad8a893f34c01d5
denisedelbando <denise.delbando@gmail.com>
2021-05-02 17:41:49 -0400

    users

--- server.js
+++ server.js
@@ -2,20 +2,22 @@ require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
 
-
 const app = express();
 
 //* CONNECT DB
 connectDB();
 
+//* MIDDLEWARE
+//get the data in request.body
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running'));
 
-//* DEFINE ROUTES 
+//* DEFINE ROUTES
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/auth', require('./routes/api/auth'));
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
